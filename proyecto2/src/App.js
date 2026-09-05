1492ceee2d379e996d0b89b090ee1a8fee76d3ed
Joel <diojo25el@gmail.com>
2026-09-05 00:43:28 -0600

    Sprint Planning en React + Documentación Scrum

---
+++ proyecto2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SprintPlanning from "./scrum/SprintPlanning";
+
+function App() {
+  return (
+    <div>
+      <SprintPlanning />
+    </div>
+  );
+}
+
+export default App;
